894856736b97d2fa0cc771d0130e03efed974658
jesica.nugnez <jesica.nugnez@gmail.com>
2022-12-11 23:38:35 +0100

    improvement - Historic container populated, total in and out containers working, total savings container working - styles responsiveness improvement

--- index.js
+++ index.js
@@ -2,41 +2,73 @@
 //* 2. Podremos añadir un ingreso o un gasto incluyendo un concepto.
 //* 3. Podremos borrar cualquier gasto o ingreso que hayamos introducido.
 
-// hago querySelector() a un nodo del document y lo meto en una variable
-const addTransactionToHistoric = () => {
-  const amountValue = document.getElementById('amount-input').value
-  
-  // Get parent node by querySelector() and adign it to a variable
-  const transactionsList = document.querySelector('.transactions-list')
-  
-  // Create a "li" node
-  const listItem = document.createElement('li');
-  
-  // Create a text node
-  const listText = document.createTextNode(amountValue);
+
+//* Creating function to add new transactions to Historic container
+
+  const addTransactionToHistoric = () => {
+
+    // Get parent node with querySelector() and asign it to a variable
+    const transactionsList = document.querySelector('.transactions-list')
+    
+    // Get value with getElementById().value
+    const amountValue = document.getElementById('amount-input').value
+    
+    // Create a "li" node
+    const listItem = document.createElement('li');
+    
+    // Create a text node
+    const listText = document.createTextNode(amountValue);
 
   // Append the text node to the "li" node
   listItem.appendChild(listText)
+
   // Append the "li" node to the list
   transactionsList.appendChild(listItem)
 
   // Call function for validating transaction type (ins or outs)
   transactionType()
+  totalSavings()
 }
 
-// Creating function to validate transaction type (ins or outs)
+
+//* Creating function to validate transaction type (ins or outs)
+
 const transactionType = () => {
-  
-  // Get parent node with querySelector()
-  const parentNode = document.getElementById('total-in');
-  
-  // Create element <span>
-  let newValue = document.createElement('span');
 
-  // Editing <span> textContent for checking changes are applying
-  newValue.textContent="5";
+  // Get each value of the li elements, evaluate if they are In or Out
+  let nodeList = document.getElementById("historic-container").getElementsByTagName("li");
+  let totalIn = 0;
+  let totalOut = 0;
+  for (let i = 0; i < nodeList.length; i++) {
+    let amount = parseInt(nodeList[i].textContent)
+    if(amount < 0) {
+      totalOut += amount  
+    } else {
+      totalIn += amount
+    }
+  }
 
-  // Append newValue to parentNode
-  parentNode.appendChild(newValue);
+  // Get parent node with getElementById()
+  let newInValue = document.getElementById('total-in-value')
+  let newOutValue = document.getElementById('total-out-value')
+
+  // Edit <span> textContent for checking that changes are applying
+  newInValue.textContent = totalIn;
+  newOutValue.textContent = totalOut;
 } 
+
+//* Creating function to populate Total Savings container 
+
+const totalSavings = () => {
+  let totalSaveValue = document.getElementById('total-savings-value');
+  let totalInVal = parseInt(document.getElementById('total-in-value').textContent);
+  let totalOutVal = parseInt(document.getElementById('total-out-value').textContent);
+  totalSaveValue.textContent = totalInVal + totalOutVal
+}
+
+
+
+
+
+
 
